294a75bf7d151102f9e9c821832d2a7708c5aef3
Paul Gowder <paul.gowder@gmail.com>
2020-06-08 15:46:30 -0500

    found the problem with newer cases, no bound volumes of us reporter after 572 still

--- fetch_case/fetch_case/fetch_case.py
+++ fetch_case/fetch_case/fetch_case.py
@@ -6,6 +6,22 @@ import lawpy
 args = sys.argv
 
 
+def backup_get_case(filename, cite):
+    cap = os.environ["CASELAW"]
+    auth = "Token " + cap
+    endpoint = "https://api.case.law/v1/cases/"
+    try: 
+        resp = requests.get(endpoint,
+                            headers={"Authorization": auth},
+                            params = {"cite": cite, "full_case": True})
+        opinion = "\n\n".join([x["text"] for x in resp.json()["results"][0]["casebody"]["data"]["opinions"]])
+        with open(filename, "w") as outfile:
+            outfile.write(opinion)
+            print("case saved!")
+    except:
+        print("CAP failing too.  Giving up.")
+
+
 def get_case(casename, cite):
     sess = lawpy.courtlistener()
     fetched = sess.fetch_cases_by_cite(cite)
@@ -16,7 +32,8 @@ def get_case(casename, cite):
             outfile.write(opinion)
             print("case saved!")
     except:
-        print("can't get opinion for cite " + cite)
+        print("courtlistener failed, trying CAP")
+        backup_get_case(filename, cite)
 
 try:
     casename = args[1]
